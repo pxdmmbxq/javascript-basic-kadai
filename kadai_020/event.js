2a2da7cbc4675a6a4d18db11e9c29825d67f8bc9
pxdmmbxq <px.o1nulo.xd@gmail.com>
2025-03-21 23:48:39 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -2,8 +2,7 @@ const btn = document.getElementById('btn');
 
 btn.addEventListener('click', () => {
   
-  const text = document.getElementById('text').textContent; 
-  document.getElementById('text').textContent = 'ボタンをクリックしました';
+  const text = document.getElementById('text'); 
+  text.textContent = 'ボタンをクリックしました';
   
 });
-
